53e9fad187cec4cefb6e5bf81b492ee3a5d6a2aa
Kocsis Máté <73794121+R4zyl@users.noreply.github.com>
2021-09-12 12:18:40 +0200

    languageSwitcher component upgraded for better UX, and usability

--- Frontend/src/components/Header/LanguageSwitcher.js
+++ Frontend/src/components/Header/LanguageSwitcher.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import engImg from '../../assets/eng.png';
 import hunImg from '../../assets/hun.png';
@@ -10,35 +10,77 @@ import {
 export const LanguageSwitcher = ({ height, className }) => {
   const { language } = useSelector((state) => state.language);
   const dispatch = useDispatch();
+  const [flagStyle, setFlagStyle] = useState({
+    filter: 'grayscale(1) brightness(99%)',
+    transition: 'filter 0.5s ease-in-out',
+  });
 
   const handleEngClick = () => {
     dispatch(changeLanguageToEngAction());
+    setFlagStyle(marked);
   };
   const handleHunClick = () => {
     dispatch(changeLanguageToHunAction());
+    setFlagStyle(marked);
   };
-          
+  const handleMouseEnter = () => {
+    setFlagStyle(hovered);
+  };
+
+  const handleMouseLeave = () => {
+    setFlagStyle(marked);
+  };
+
+  const marked = {
+    filter: 'grayscale(1) brightness(70%)',
+    transition: 'filter 0.7s ease-in-out',
+  };
+  const hovered = {
+    filter: 'grayscale(0.2) brightness(94%)',
+  };
   return (
-     
     <div>
-       {language === 'hun' ? (
-        <img
-          className={className}
-          src={engImg}
-          onClick={handleEngClick}
-          height={height}
-          alt="váltás magyar nyelvre"
-        ></img>
+      {language === 'hun' ? (
+        <div>
+          <img
+            className={className}
+            src={hunImg}
+            onClick={handleHunClick}
+            height={height}
+            alt="switch language to english"
+          ></img>
+          <img
+            className={className}
+            src={engImg}
+            onClick={handleEngClick}
+            height={height}
+            style={flagStyle}
+            onMouseEnter={handleMouseEnter}
+            onMouseLeave={handleMouseLeave}
+            alt="váltás magyar nyelvre"
+          ></img>
+        </div>
       ) : (
-        <img
-        className={className}
-          src={hunImg}
-          onClick={handleHunClick}
-          height={height}
-          alt="switch language to english"
-        ></img>
+        <div>
+          <img
+            className={className}
+            src={hunImg}
+            onClick={handleHunClick}
+            height={height}
+            style={flagStyle}
+            onMouseEnter={handleMouseEnter}
+            onMouseLeave={handleMouseLeave}
+            alt="switch language to english"
+          ></img>
+          <img
+            className={className}
+            src={engImg}
+            onClick={handleEngClick}
+            height={height}
+            alt="váltás magyar nyelvre"
+          ></img>
+        </div>
       )}
     </div>
   );
-
 };
